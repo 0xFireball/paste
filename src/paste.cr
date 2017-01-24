ad903307c99254756e2cc67053f7991aff9b7c30
0xFireball <0xFireball@outlook.com>
2017-01-23 16:47:04 -0800

    add default port

--- src/paste.cr
+++ src/paste.cr
@@ -1,5 +1,9 @@
 require "./paste/*"
+require "kemal"
+
+default_port = 5020
+port = ENV.has_key?("PORT") ? ENV.fetch("PORT").as?(Int32) : default_port || default_port
 
 module Paste
-  
+  Kemal.run port
 end
