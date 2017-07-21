7e7437c0302cd8d881905a43d6092163b3a2cc54
0xFireball <0xFireball@outlook.com>
2017-07-20 20:42:06 -0700

    update default port logic

--- src/paste.cr
+++ src/paste.cr
@@ -2,7 +2,7 @@ require "./paste/*"
 require "kemal"
 
 default_port = 5020
-port = ENV.has_key?("PORT") ? ENV.fetch("PORT").as?(Int32) : default_port || default_port
+port = ENV.has_key?("PORT") ? ENV.fetch("PORT").as?(Int32) : default_port
 
 module Paste
   Kemal.run port
