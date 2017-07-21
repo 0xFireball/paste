e5dbf6dfe81142f5dde60627f77cdbbfa2fe5912
0xFireball <0xFireball@outlook.com>
2017-07-20 20:46:08 -0700

    add moonjs client

--- src/paste/modules/home.cr
+++ src/paste/modules/home.cr
@@ -1,6 +1,9 @@
 require "kemal"
 require "kilt/slang"
 
+serve_static({"gzip" => true, "dir_listing" => true})
+public_folder("./client/dist")
+
 get "/" do
   render "./client/dist/index.html"
 end
